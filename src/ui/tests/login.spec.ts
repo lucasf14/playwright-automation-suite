18ceafcbf9bd211632cb0d6e0c69e31e324b1651
Lucas Ferreira <lucassky14@gmail.com>
2026-03-15 17:53:33 +0000

    Add failed login tests for invalid credentials and empty fields

--- src/ui/tests/login.spec.ts
+++ src/ui/tests/login.spec.ts
@@ -19,3 +19,31 @@ test('Successful login', async ({ page }) => {
   await expect(widgets.first()).toBeVisible();
   expect(await widgets.count()).toBeGreaterThan(1);
 });
+
+
+test('Unsuccessful login with invalid credentials', async ({ page }) => {
+  const errorMessage = page.locator('.oxd-alert-content-text');
+  
+  const loginPage = new LoginPage(page);
+  await loginPage.goto(process.env.UI_BASE_URL || '');
+  await loginPage.login('qwerty', '123456');
+
+  await expect(errorMessage).toBeVisible();
+  await expect(errorMessage).toHaveText('Invalid credentials');
+});
+
+
+test('Unsuccessful login with empty fields', async ({ page }) => {  
+  const usernameContainer = page.locator('.oxd-input-group', { hasText: 'Username' });
+  const passwordContainer = page.locator('.oxd-input-group', { hasText: 'Password' });
+  const usernameRequired = usernameContainer.locator('.oxd-input-field-error-message'); 
+  const passwordRequired = passwordContainer.locator('.oxd-input-field-error-message');
+  
+  const loginPage = new LoginPage(page);
+  await loginPage.goto(process.env.UI_BASE_URL || '');
+  await loginPage.login('', '');
+
+  await expect(usernameRequired).toHaveText('Required');
+  await expect(passwordRequired).toHaveText('Required');
+});
+
